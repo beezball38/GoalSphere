23f8f604479829611f64c91017548fd3131632b3
beserchris <beserchris@gmail.com>
2025-05-26 19:33:01 -0700

    Using ULIDs now for better DB performance without having to use sequential ints. Still converts to Guid but entity framework knows how to handle it

--- GoalSettingApp.SharedKernel/Entities/BaseEntity.cs
+++ GoalSettingApp.SharedKernel/Entities/BaseEntity.cs
@@ -2,6 +2,6 @@ namespace GoalSettingApp.SharedKernel.Entities;
 
 public abstract class BaseEntity
 {
-    public Guid Id { get; set; } 
+    public Guid Id { get; set; } = Ulid.NewUlid().ToGuid(); 
     public DateTime CreatedTime { get; set; }
 }
